9970c98a3842f29be65d789fb835c8a544d93dd8
Aleksandr <alex.komissarov@cloudblue.com>
2022-11-14 14:28:10 +0100

    v0.7.16 - change of event titles

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@
 *   [LICENSE](./LICENSE).
 
 ## CHANGELOG
+*  0.7.16: Change of titles  
 *  0.7.15: Fixed error that appears on empty conversation  
 *  0.7.14: Making linter happy again  
 *  0.7.13: No failed messages on "Token not set" error anymore  
--- connect_telegram_ext/titles.py
+++ connect_telegram_ext/titles.py
@@ -7,12 +7,12 @@
 # Copyright (c) 2022, Cloudblue Connect
 # All rights reserved.
 #
-ASSET_ADJUSTMENT_REQUEST_PROCESSING = 'Asset adjustment request processing'
-ASSET_CANCEL_REQUEST_PROCESSING = 'Asset cancel request processing'
-ASSET_CHANGE_REQUEST_PROCESSING = 'Asset change request processing'
-ASSET_PURCHASE_REQUEST_PROCESSING = 'Asset purchase request processing'
-ASSET_RESUME_REQUEST_PROCESSING = 'Asset resume request processing'
-ASSET_SUSPEND_REQUEST_PROCESSING = 'Asset suspend request processing'
+ASSET_ADJUSTMENT_REQUEST_PROCESSING = 'Subscription adjustment request processing'
+ASSET_CANCEL_REQUEST_PROCESSING = 'Subscription cancel request processing'
+ASSET_CHANGE_REQUEST_PROCESSING = 'Subscription change request processing'
+ASSET_PURCHASE_REQUEST_PROCESSING = 'Subscription purchase request processing'
+ASSET_RESUME_REQUEST_PROCESSING = 'Subscription resume request processing'
+ASSET_SUSPEND_REQUEST_PROCESSING = 'Subscription suspend request processing'
 
 TIER_ACCOUNT_UPDATE_REQUEST_PROCESSING = 'Tier account update request processing'
 TIER_CONFIG_ADJUSTMENT_REQUEST_PROCESSING = 'Tier config adjustment request processing'
@@ -21,7 +21,7 @@ TIER_CONFIG_SETUP_REQUEST_PROCESSING = 'Tier config setup request processing'
 
 INSTALLATION_STATUS_CHANGE_PROCESSING = 'Installation status change'
 
-PART_USAGE_FILE_REQUEST_PROCESSING = 'Part usage file request processing'
+PART_USAGE_FILE_REQUEST_PROCESSING = 'Chunk usage file request processing'
 USAGE_FILE_REQUEST_PROCESSING = 'Usage file request processing'
 USAGE_FILE_CREATION_PROCESSING = ''
 USAGE_FILE_UPLOAD_PROCESSING = ''
